6c424762b00c0f688d76c51a7b20afa94a488541
Viveka-ravi <52704725+Viveka-ravi@users.noreply.github.com>
2019-07-12 16:02:19 +0530

    roman.py

---
+++ roman.py
@@ -0,0 +1,4 @@
+num = input()
+list = ["I" , "II" , "III" , "IV" , "V" , "VI" , "VII" , "VIII" , "IX" , "X" , "XI" , "XII" , "XIII" , "XIV" , "XV" , "XVI" , "XVII" , "XVIII" , "XIX", "XX"]
+for num in list
+  print(
