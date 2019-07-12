5add13decec60a6933ac960fe37f872301a84644
Viveka-ravi <52704725+Viveka-ravi@users.noreply.github.com>
2019-07-12 15:18:24 +0530

    num_reverse.py

---
+++ num_reverse.py
@@ -0,0 +1,15 @@
+str = int(input())
+str = list(str)
+str_2 = ""
+length = len(str)
+leng = length-1
+if (length == 0):
+  length = len(str)//2
+else:
+  length = len(str)//2+1
+for i in range(0,length):
+  temp = str[i]
+  str[i] = str[leng - i]
+  str[leng - i] = temp
+str_2 = str_2.join(str)
+print(int(str_2))
