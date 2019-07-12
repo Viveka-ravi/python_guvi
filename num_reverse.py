09e25abe24db83dbe99b3ffe914ab994dabf6b48
Viveka-ravi <52704725+Viveka-ravi@users.noreply.github.com>
2019-07-12 15:23:52 +0530

    Update num_reverse.py

--- num_reverse.py
+++ num_reverse.py
@@ -1,15 +1,15 @@
-str = int(input())
-str = list(str(str))
+str_1 = int(input())
+str_1 = list(str(str_1))
 str_2 = ""
-length = len(str)
+length = len(str_1)
 leng = length-1
 if (length%2 == 0):
-  length = len(str)//2
+  length = len(str_1)//2
 else:
-  length = len(str)//2+1
+  length = len(str_1)//2+1
 for i in range(0,length):
-  temp = str[i]
-  str[i] = str[leng - i]
-  str[leng - i] = temp
-str_2 = str_2.join(str)
+  temp = str_1[i]
+  str_1[i] = str_1[leng - i]
+  str_1[leng - i] = temp
+str_2 = str_2.join(str_)
 print(int(str_2))
