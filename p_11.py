f07cfaec3317e4008bfa8a7081a35ddc87d31c97
Viveka-ravi <52704725+Viveka-ravi@users.noreply.github.com>
2019-07-14 22:54:47 +0530

    Update p_11.py

--- p_11.py
+++ p_11.py
@@ -1,7 +1,10 @@
 num = int(input())
 fact = 1
-num_1 = num-1
-for i in range(1,num_1+1): 
-    fact = fact * i 
-      
-print (fact)
+if(num%2 == 0):
+    num_1 = num-1
+    for i in range(1,num_1+1): 
+        fact = fact * i 
+    print (fact)
+else:
+    print(num)
+        
