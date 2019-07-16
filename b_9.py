a4b47d5885701b563f80e80744e696453debbcb4
Viveka-ravi <52704725+Viveka-ravi@users.noreply.github.com>
2019-07-16 12:08:49 +0530

    Update b_9.py

--- b_9.py
+++ b_9.py
@@ -1,6 +1,7 @@
 n, p = map(int,input().split())
 s = list(input().split())
-for i in range(0, ):
-   
-print(s)
-    
+s = [int(x) for x in s]
+sum = 0
+for i in range(0,p):
+   sum = sum + s[i]
+print(sum)
