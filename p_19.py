78d717dc3c6ff4fe51bb52f67930c9f98c8838de
Viveka-ravi <52704725+Viveka-ravi@users.noreply.github.com>
2019-07-16 10:28:34 +0530

    Update p_19.py

--- p_19.py
+++ p_19.py
@@ -2,9 +2,12 @@ n = int(input())
 s1 = []
 for i in range(0,n):
     s = list(input().split())
-    s1 = s + s1        
+    s1 = s1 + s        
 
-
+print(s1)
+s1=[int(x) for x in s1]
+#print(s1)
 s1.sort()
-res = (' '.join(s1))
-print(res,end=" ")
+for x in s1:
+    print(x,end=" ")
+
